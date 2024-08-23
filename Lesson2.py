f4c6839adef77cecb22f74ecdf74ab9f81c89361
NightmareWitch <jokerhell13@gmail.com>
2024-08-23 10:09:23 +0300

    Add files via upload

--- Lesson2.py
+++ Lesson2.py
@@ -1,13 +1,13 @@
 #Количество задач
-a = 12
-print(a)
+number_of_tasks = 12
+print(number_of_tasks)
 #Затраченное время
-b = float(1.5)
-print(b)
+time_spent = float(1.5)
+print(time_spent)
 #Курс
 name = 'Phyton'
 print(name)
 #Время на одно выполнение
-c = b / a
-print(c)
-print('Курс' , name,',всего задач', a,',затраченное время' , b,',время на одно задание' , c)
+time_per_task = time_spent / number_of_tasks
+print(time_per_task)
+print('Курс' , name,',всего задач', number_of_tasks,',затраченное время' , time_spent,',время на одно задание' , time_per_task)
